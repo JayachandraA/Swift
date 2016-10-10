bf2a346e72c6dfc6fae23d8d428b8be8fa3291c2
Jayachandra Agraharam <jayachandra@Jayachandras-MacBook-Pro.local>
2016-10-10 12:11:38 +0530

    DotedView class file added

---
+++ DotedView.swift
@@ -0,0 +1,111 @@
+//
+//  DotedView.swift
+//  DotedView
+//
+//  Created by Jayachandra Agraharam on 10/10/16.
+//  Copyright © 2016 Webterminal Inc. All rights reserved.
+//
+
+
+
+
+import UIKit
+
+@IBDesignable
+open class DotedView: UIView {
+    
+    @IBInspectable
+    open var lineColor: UIColor = UIColor.black
+    
+    @IBInspectable
+    open var lineWidth: CGFloat = CGFloat(4)
+    
+    @IBInspectable
+    open var round: Bool = false
+    
+    @IBInspectable
+    open var horizontal: Bool = true
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        initBackgroundColor()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initBackgroundColor()
+    }
+    
+    override open func prepareForInterfaceBuilder() {
+        initBackgroundColor()
+    }
+    
+    override open func draw(_ rect: CGRect) {
+        
+        let path = UIBezierPath()
+        path.lineWidth = lineWidth
+        
+        if round {
+            configureRoundPath(path, rect: rect)
+        } else {
+            configurePath(path, rect: rect)
+        }
+        
+        lineColor.setStroke()
+        
+        path.stroke()
+    }
+    
+    func initBackgroundColor() {
+        if backgroundColor == nil {
+            backgroundColor = UIColor.clear
+        }
+    }
+    
+    fileprivate func configurePath(_ path: UIBezierPath, rect: CGRect) {
+        if horizontal {
+            let center = rect.height * 0.5
+            let drawWidth = rect.size.width - (rect.size.width.truncatingRemainder(dividingBy: (lineWidth * 2))) + lineWidth
+            let startPositionX = (rect.size.width - drawWidth) * 0.5 + lineWidth
+            
+            path.move(to: CGPoint(x: startPositionX, y: center))
+            path.addLine(to: CGPoint(x: drawWidth, y: center))
+            
+        } else {
+            let center = rect.width * 0.5
+            let drawHeight = rect.size.height - (rect.size.height.truncatingRemainder(dividingBy: (lineWidth * 2))) + lineWidth
+            let startPositionY = (rect.size.height - drawHeight) * 0.5 + lineWidth
+            
+            path.move(to: CGPoint(x: center, y: startPositionY))
+            path.addLine(to: CGPoint(x: center, y: drawHeight))
+        }
+        
+        let dashes: [CGFloat] = [lineWidth, lineWidth]
+        path.setLineDash(dashes, count: dashes.count, phase: 0)
+        path.lineCapStyle = CGLineCap.butt
+    }
+    
+    fileprivate func configureRoundPath(_ path: UIBezierPath, rect: CGRect) {
+        if horizontal {
+            let center = rect.height * 0.5
+            let drawWidth = rect.size.width - (rect.size.width.truncatingRemainder(dividingBy: (lineWidth * 2)))
+            let startPositionX = (rect.size.width - drawWidth) * 0.5 + lineWidth
+            
+            path.move(to: CGPoint(x: startPositionX, y: center))
+            path.addLine(to: CGPoint(x: drawWidth, y: center))
+            
+        } else {
+            let center = rect.width * 0.5
+            let drawHeight = rect.size.height - (rect.size.height.truncatingRemainder(dividingBy: (lineWidth * 2)))
+            let startPositionY = (rect.size.height - drawHeight) * 0.5 + lineWidth
+            
+            path.move(to: CGPoint(x: center, y: startPositionY))
+            path.addLine(to: CGPoint(x: center, y: drawHeight))
+        }
+        
+        let dashes: [CGFloat] = [0, lineWidth * 2]
+        path.setLineDash(dashes, count: dashes.count, phase: 0)
+        path.lineCapStyle = CGLineCap.round
+    }
+    
+}
